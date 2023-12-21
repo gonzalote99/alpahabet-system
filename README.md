1d6debb9935236e6e41ab626d323b4eed1495842
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-12-21 16:23:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# alpahabet-system
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/gonzalote99/alpahabet-system)
---
+++ script.js
@@ -0,0 +1,80 @@
+var image = document.querySelector('#alphabet-image');
+document.querySelector('#a').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_a.jpeg'
+})
+document.querySelector('#b').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_b.jpeg'
+})
+document.querySelector('#c').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_c.jpeg'
+})
+document.querySelector('#d').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_d.jpeg'
+})
+document.querySelector('#e').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_e.jpeg'
+})
+document.querySelector('#f').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_f.jpeg'
+})
+document.querySelector('#g').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_g.jpeg'
+})
+document.querySelector('#h').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_h.jpeg'
+})
+document.querySelector('#i').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_i.jpeg'
+})
+document.querySelector('#j').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_j.jpeg'
+})
+document.querySelector('#k').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_k.jpeg'
+})
+document.querySelector('#l').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_l.jpeg'
+})
+document.querySelector('#m').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_m.jpeg'
+})
+document.querySelector('#n').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_n.jpeg'
+})
+document.querySelector('#o').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_o.jpeg'
+})
+document.querySelector('#p').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_p.jpeg'
+})
+document.querySelector('#q').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_q.jpeg'
+})
+document.querySelector('#r').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_r.jpeg'
+})
+document.querySelector('#s').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_s.jpeg'
+})
+document.querySelector('#t').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_t.jpeg'
+})
+document.querySelector('#u').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_u.jpeg'
+})
+document.querySelector('#v').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_v.jpeg'
+})
+document.querySelector('#w').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_w.jpeg'
+})
+document.querySelector('#x').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_x.jpeg'
+})
+document.querySelector('#y').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_y.jpeg'
+})
+
+document.querySelector('#z').addEventListener('click', function() {
+  image.src = 'https://raw.githubusercontent.com/mubarakumn/alphabets/main/images/alpha_z.jpeg'
+})
